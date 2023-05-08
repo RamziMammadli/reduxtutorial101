c42675e0da3aa41dada959e68cb1aa2575221357
RamziMammadli <remzimemmedli95@gmail.com>
2023-05-09 00:53:22 +0400

    reduxtoolkit

---
+++ src/redux/reducers/postSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const postSlice = createSlice({
+    name: 'post',
+    initialState:{
+        ad,
+        soyad,
+        yas
+    },
+    reducers:{},
+    extraReducers:
+})
